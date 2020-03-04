b1f0e6ac549a84fe83a3a616125a5e81612587b4
daehyun_kang <daehyun_kang@tmax.co.kr>
2020-03-04 15:46:42 +0900

    add "hell_electron" test example (with nodeIntegration false option), add navie debug mode (openDevTools), add test icon in example/top

---
+++ index_debug.js
@@ -0,0 +1,16 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+
+  win.loadFile('index.html')
+  win.webContents.openDevTools() 
+}
+
+app.whenReady().then(createWindow)
